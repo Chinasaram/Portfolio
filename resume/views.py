4b992dbdb5978beae146d597df44aa0079596c24
chinasaram <anelechinasaram@gmail.com>
2021-08-20 01:39:33 +0100

    portfolio app

---
+++ resume/views.py
@@ -0,0 +1,21 @@
+from resume.models import Contact
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+
+# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        contact.name = name
+        contact.email = email
+        contact.subject = subject
+        contact.message = message
+        contact.save()
+        return HttpResponse('<h2>Thank you for contacting me, I will get back to you shortly.</h2>')
+    return render(request, 'resume/home.html')
