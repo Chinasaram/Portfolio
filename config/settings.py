7ad0d3a1d11f831337667d1848a82be92c27c96e
chinasaram <anelechinasaram@gmail.com>
2021-09-23 17:42:11 +0100

    resolving server error

--- config/settings.py
+++ config/settings.py
@@ -33,9 +33,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'FALSE'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
