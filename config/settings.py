d6d17e0b114d2881429ed79e73422dc32137a5b3
chinasaram <anelechinasaram@gmail.com>
2021-09-23 17:18:55 +0100

    resolving server error

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = str(BASE_DIR.joinpath('static'))
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
